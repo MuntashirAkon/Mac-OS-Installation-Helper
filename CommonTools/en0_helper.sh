ca44b7a6e2be26cc220039faf98d0304afa09c60
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2016-01-29 09:53:02 +0600

    Create en0_helper.sh

---
+++ CommonTools/en0_helper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "en0 Configuration Helper"
+echo "------------------------"
+echo -n "Do you have a working Ethernet/Wifi? (y/n): "
+input confirm
+if [ $confirm = "y" ]; then
+  # delete network interfaces
+  cd /Library/Preferences/SystemConfiguration/
+  rm NetworkInterfaces.plist
+  rm preferences.plist
+else
+  echo "You have to use Null Ethernet Network. Visit this link for details: http://bit.ly/null-ethernet"
+  exit
+fi
+echo "Done. Restart? (y/n): "
+input confirm
+if [ $confirm = "y" ]; then
+  sudo shutdown -r now "Restarting..."
+fi
+echo "You choose not to restart now."
+exit 0
